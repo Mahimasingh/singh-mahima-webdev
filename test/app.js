ad02f947f372b50b6bfb929588d3a17902debbdd
Mahima Singh <Mahima Singh>
2017-07-05 08:22:41 -0400

    Extra folder deleted

--- test/app.js
+++ test/app.js
@@ -6,13 +6,22 @@ app.delete("/api/test/:id", deleteMessage);
 
 var connectionString = 'mongodb://127.0.0.1:27017/test';
 
-if(process.env.MLAB_USERNAME) {
-    connectionString = process.env.MLAB_USERNAME + ":" +
-        process.env.MLAB_PASSWORD + "@" +
-        process.env.MLAB_HOST + ':' +
-        process.env.MLAB_PORT + '/' +
-        process.env.MLAB_APP_NAME;
+
+if(process.env.MLAB_USERNAME_WEBDEV) { // check if running remotely
+    var username = process.env.MLAB_USERNAME_WEBDEV; // get from environment
+    var password = process.env.MLAB_PASSWORD_WEBDEV;
+    connectionString = 'mongodb://' + username + ':' + password;
+    connectionString += '@ds149412.mlab.com:49412/heroku_glmqj56d'; // user yours
 }
+
+
+//if(process.env.MLAB_USERNAME) {
+//    connectionString = process.env.MLAB_USERNAME + ":" +
+//        process.env.MLAB_PASSWORD + "@" +
+//        process.env.MLAB_HOST + ':' +
+//        process.env.MLAB_PORT + '/' +
+//        process.env.MLAB_APP_NAME;
+//}
 
 var mongoose = require("mongoose");
 mongoose.connect(connectionString);
