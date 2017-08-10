ab06342e1efe5846c8690f1c985bbab6b0a03a7e
Mahima Singh <mahi.si1992@gmail.com>
2017-08-09 21:10:01 -0400

    setting mlab

--- assignment/models/database.js
+++ assignment/models/database.js
@@ -1,13 +1,6 @@
 var q = require('q');
 
-var connectionString = 'mongodb://127.0.0.1:27017/webdev_summer2_2017'; // for local
-if(process.env.MLAB_USERNAME_WEBDEV) { // check if running remotely
-    var username = process.env.MLAB_USERNAME_WEBDEV; // get from environment
-    var password = process.env.MLAB_PASSWORD_WEBDEV;
-    connectionString = 'mongodb://' + username + ':' + password;
-    connectionString += '@ds149412.mlab.com:49412/heroku_glmqj56d'; // user yours
-}
-
+var connectionString = process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/webdev_summer2_2017'; // for local
 
 var mongoose = require("mongoose");
 var db = mongoose.connect(connectionString);
