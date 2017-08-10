fe3b0608956263264149730cdafffc32d101b3b6
Mahima Singh <mahi.si1992@gmail.com>
2017-08-09 21:16:16 -0400

    database fixing

--- assignment/models/database.js
+++ assignment/models/database.js
@@ -1,7 +1,5 @@
 var q = require('q');
 
-var connectionString = process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/webdev_summer2_2017'; // for local
-
 var mongoose = require("mongoose");
 var db = mongoose.connect(connectionString);
 mongoose.Promise = q.Promise;
